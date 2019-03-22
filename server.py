304ae68a4d941952b103d2da65a0b2915f4acb0b
Anders Jakobsen <andersjkbsn@gmail.com>
2019-03-22 14:16:49 +0100

    JSON

--- server.py
+++ server.py
@@ -1,28 +1,38 @@
 import asyncio
-
+import json
 import websockets
 from loguru import logger
 
 clients = dict()
 
 
+class Client:
+    def __init__(self, name):
+        self.name = name
+
 async def handle_messages(websocket):
     while True:
-        message = (await websocket.recv()).strip()
+        packet = json.loads(await websocket.recv())
+        logger.info(f'Received {packet}')
 
-        logger.info(f'Received {message}')
+
+        if packet['type'] != 'message':
+            continue
+
+        message = packet['message']
 
         others = [socket for socket in clients if socket != websocket]
         if others:
-            name = clients[websocket]
-            await asyncio.wait([socket.send(f'{name},{message}') for socket in others])
+            packet = json.dumps(dict(type='message', author=clients[websocket].name, message=message))
+            logger.info(packet)
+            await asyncio.wait([socket.send(packet) for socket in others])
 
 
 async def handle_client(websocket, path):
-    name = (await websocket.recv()).strip()
-    clients[websocket] = name
+    packet = json.loads(await websocket.recv())
+    clients[websocket] = Client(packet['name'])
 
-    logger.info(f'New connection from {name}')
+    logger.info(f'New connection from {clients[websocket].name}')
 
     try:
         await handle_messages(websocket)
