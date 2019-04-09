e707b2541800915bf94805d6968ef43db7c4ff9b
Anders Jakobsen <andersjkbsn@gmail.com>
2019-04-09 12:24:59 +0200

    Fixes #2

--- client.py
+++ client.py
@@ -1,26 +1,37 @@
+import asyncio
 import json
 from loguru import logger
 
+
 class Client:
-    def __init__(self, name, avatar, channel, websocket):
-        self.name = name
-        self.avatar = avatar
+    def __init__(self, server, websocket):
         self.websocket = websocket
-        self.channel = channel
+        self.server = server
+
+        self.name = None
+        self.channel = None
+        self.avatar = None
+
+    async def receive_packet(self):
+        return json.loads(await self.websocket.recv())
 
     async def handle_messages(self):
+        handlers = {'message': self.handle_chat_message,
+                    'identity': self.handle_identity}
+
         while True:
             packet = json.loads(await self.websocket.recv())
             logger.info(f'Received {packet}')
 
-            if packet['type'] != 'message':
-                continue
+            method = handlers.get(packet['type'], None)
+            if method:
+                await method(packet)
 
-            message = packet['message']
+    async def handle_chat_message(self, packet):
+        packet = json.dumps(dict(type='message', author=self.name, avatar=self.avatar, message=packet['message']))
+        await self.server.broadcast(packet, self.channel, [self])
 
-            others = [socket for socket in clients if socket != websocket]
-            if others:
-                packet = json.dumps(
-                    dict(type='message', author=clients[websocket].name, avatar=clients[websocket].avatar, message=message))
-                logger.info(packet)
-                await asyncio.wait([socket.send(packet) for socket in others])
+    async def handle_identity(self, packet):
+        self.name = packet['name']
+        self.avatar = packet['avatar']
+        self.channel = packet['channel']
